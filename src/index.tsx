5a1dc357fcae9c2b38a2ed6a8a20c050f950aa7a
Nikola Malovic <malovicn@papiri.rs>
2021-08-16 14:47:41 +0200

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import addonSdk, {
+  LogLevel,
+  EventOrigin,
+  EventType,
+} from "@outreach/client-addon-sdk";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let counter = 0;
+
+addonSdk.init().then((ctx) => {
+  console.debug("[HelloWorld] addonSdk.initialized", { ctx });
+
+  setTimeout(() => {
+    
+    addonSdk.decorate((counter++).toString(), "badge");
+  }, 10 * 1000);
+});
